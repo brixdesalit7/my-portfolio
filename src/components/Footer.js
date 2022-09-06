8a54d34c4efb38c31c46f505a95c3ea9e2ee06df
brixdesalit7 <brixdesalit7@gmail.com>
2022-09-06 19:04:29 +0800

    m

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const Footer = () => {
-
   return (
     <div className='footer' id="contact">
     <div className='footer-content'>
@@ -10,10 +9,10 @@ const Footer = () => {
         </div>
         <div className='footer-icons'>
             <ul>
-                {/* <li><a href="https://www.linkedin.com/in/bryxx-andre-desalit-559680249/" target="_blank"><i class="bi bi-linkedin"></i></a></li>
+                <li><a href="https://www.linkedin.com/in/bryxx-andre-desalit-559680249/" target="_blank"><i class="bi bi-linkedin"></i></a></li>
                 <li><a href="https://www.facebook.com/xxyrb" target="_blank"><i class="bi bi-facebook"></i></a></li>
                 <li><a href="https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZXhxtRXTMNNcNWVhMrGbnlWhmZKZjJlBVlChjFJBtzPCShDTjcRfBXtLWQcplZRbkpbFg" target="_blank"><i class="bi bi-google"></i></a></li>
-                <li><a href="https://github.com/brixdesalit7" target="_blank"><i class="bi bi-github"></i></a></li> */}
+                <li><a href="https://github.com/brixdesalit7" target="_blank"><i class="bi bi-github"></i></a></li>
             </ul>
         </div>
         <div className='footer-tagline'>
